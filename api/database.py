fecb1acb323be82cad4dc6f5e1a426b07396e5ae
Caio <caioalmeidamendonca@gmail.com>
2024-09-05 17:39:34 -0300

    BANCO DE DAODS LOCAL ADICIONADO
    ...

--- api/database.py
+++ api/database.py
@@ -2,9 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./administradores.db"
+SQLALCHEMY_DATABASE_URL_ADMIN = "sqlite:///./administradores.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL_ADMIN, connect_args={"check_same_thread": False})
+
+SQLALCHEMY_DATABASE_URL_AGENDA = "sqlite:///./agenda.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL_AGENDA, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
